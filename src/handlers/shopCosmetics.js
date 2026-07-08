6c7e5369c6ab4e3b90f27493c99c1405427705e4
koshel111 <ilmaz.gaynullin123@mail.ru>
2026-07-08 21:46:36 +0300

    Исправление косметики: через reply вместо edit

--- src/handlers/shopCosmetics.js
+++ src/handlers/shopCosmetics.js
@@ -5,7 +5,8 @@ module.exports = (bot) => {
   bot.action('cosmetics_menu', async (ctx) => {
     await ctx.answerCbQuery();
     console.log('✅ Меню косметики');
-    await ctx.editMessageText('💄 *Косметика*', {
+    await ctx.deleteMessage();
+    await ctx.reply('💄 *Косметика*', {
       parse_mode: 'Markdown',
       ...Markup.inlineKeyboard([
         [Markup.button.callback('🎽 Формы', 'cosm_jerseys')],
@@ -17,21 +18,21 @@ module.exports = (bot) => {
   bot.action('cosm_jerseys', async (ctx) => {
     await ctx.answerCbQuery();
     console.log('✅ Формы открыты');
-    await ctx.editMessageText('🎽 *Формы*', {
+    await ctx.deleteMessage();
+    await ctx.reply('🎽 *Формы*', {
       parse_mode: 'Markdown',
       ...Markup.inlineKeyboard([
         [Markup.button.callback('ЦСКА (100⭐)', 'cosm_buy_csk')],
-        [Markup.button.callback('Спартак (100⭐)', 'cosm_buy_spartak')],
         [Markup.button.callback('🔙 Назад', 'cosmetics_menu')],
       ])
     });
   });
 
-  bot.action(/cosm_buy_(.+)/, async (ctx) => {
+  bot.action('cosm_buy_csk', async (ctx) => {
     await ctx.answerCbQuery();
-    const id = ctx.match[1];
-    console.log(`🔴 НАЖАТА КНОПКА: cosm_buy_${id}`);
-    await ctx.editMessageText(`✅ Ты купил: ${id}`);
+    console.log('🔴 НАЖАТА КНОПКА: cosm_buy_csk');
+    await ctx.deleteMessage();
+    await ctx.reply('✅ Ты купил: ЦСКА (тест)');
   });
 
 };
