4ef51a114bd02aca043136002e103b668e22c76b
koshel111 <ilmaz.gaynullin123@mail.ru>
2026-07-08 21:42:01 +0300

    Добавлены тестовые кнопки для форм

--- src/handlers/shopCosmetics.js
+++ src/handlers/shopCosmetics.js
@@ -21,15 +21,17 @@ module.exports = (bot) => {
       parse_mode: 'Markdown',
       ...Markup.inlineKeyboard([
         [Markup.button.callback('ЦСКА (100⭐)', 'test_buy_csk')],
+        [Markup.button.callback('Спартак (100⭐)', 'test_buy_spartak')],
         [Markup.button.callback('🔙 Назад', 'cosmetics_menu')],
       ])
     });
   });
 
-  bot.action('test_buy_csk', async (ctx) => {
+  bot.action(/test_buy_(.+)/, async (ctx) => {
     await ctx.answerCbQuery();
-    console.log('🔴 НАЖАТА КНОПКА: test_buy_csk');
-    await ctx.editMessageText('✅ Ты купил: csk (тест)');
+    const id = ctx.match[1];
+    console.log(`🔴 НАЖАТА КНОПКА: test_buy_${id}`);
+    await ctx.editMessageText(`✅ Ты купил: ${id}`);
   });
 
 };
