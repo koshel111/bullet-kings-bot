73bbf2922fdb86d5a4b64df6c6f03200ee0d76b7
koshel111 <ilmaz.gaynullin123@mail.ru>
2026-07-08 21:45:01 +0300

    Исправлен конфликт: новый префикс cosm_buy_

--- src/handlers/shopCosmetics.js
+++ src/handlers/shopCosmetics.js
@@ -8,29 +8,29 @@ module.exports = (bot) => {
     await ctx.editMessageText('💄 *Косметика*', {
       parse_mode: 'Markdown',
       ...Markup.inlineKeyboard([
-        [Markup.button.callback('🎽 Формы', 'test_jerseys')],
+        [Markup.button.callback('🎽 Формы', 'cosm_jerseys')],
         [Markup.button.callback('🔙 Назад', 'shop')],
       ])
     });
   });
 
-  bot.action('test_jerseys', async (ctx) => {
+  bot.action('cosm_jerseys', async (ctx) => {
     await ctx.answerCbQuery();
     console.log('✅ Формы открыты');
     await ctx.editMessageText('🎽 *Формы*', {
       parse_mode: 'Markdown',
       ...Markup.inlineKeyboard([
-        [Markup.button.callback('ЦСКА (100⭐)', 'test_buy_csk')],
-        [Markup.button.callback('Спартак (100⭐)', 'test_buy_spartak')],
+        [Markup.button.callback('ЦСКА (100⭐)', 'cosm_buy_csk')],
+        [Markup.button.callback('Спартак (100⭐)', 'cosm_buy_spartak')],
         [Markup.button.callback('🔙 Назад', 'cosmetics_menu')],
       ])
     });
   });
 
-  bot.action(/test_buy_(.+)/, async (ctx) => {
+  bot.action(/cosm_buy_(.+)/, async (ctx) => {
     await ctx.answerCbQuery();
     const id = ctx.match[1];
-    console.log(`🔴 НАЖАТА КНОПКА: test_buy_${id}`);
+    console.log(`🔴 НАЖАТА КНОПКА: cosm_buy_${id}`);
     await ctx.editMessageText(`✅ Ты купил: ${id}`);
   });
 
